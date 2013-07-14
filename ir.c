2ed34f32660ca51591796927f1e578ed4c2182f1
kazubu <kazubu@6c23ddd9-6acf-e011-bcb5-000c29b07eb3>
2013-07-14 08:12:57 +0000

    * create dummy functions
    git-svn-id: https://a.jtime.net/svn/misc/ruby-libir@31 6c23ddd9-6acf-e011-bcb5-000c29b07eb3

--- ir.c
+++ ir.c
@@ -2,7 +2,7 @@
 VALUE rb_cIR;
 
 VALUE meth_initialize(){
-  printf("initialize\n");
+  //TODO: initialize
 
   return INT2FIX(0);
 }
@@ -14,7 +14,7 @@ VALUE meth_sendIr(VALUE self, VALUE varr){
 
   Check_Type(varr, T_ARRAY);
 
-  for(i=0; i<length; i++){
+  for(i=0; i<length; i++){//XXX: Dummy 
     VALUE vval = rb_ary_entry(varr,i);
     Check_Type(vval, T_FIXNUM);
     int val = FIX2INT(vval);
@@ -25,9 +25,16 @@ VALUE meth_sendIr(VALUE self, VALUE varr){
   return ret;
 }
 
+VALUE meth_recvIr(VALUE self){
+  //TODO: receive
+  
+  return INT2FIX(0);
+}
+
 void Init_IR(void){
   rb_cIR = rb_define_class("IR", rb_cObject);
 
   rb_define_private_method(rb_cIR, "initialize", meth_initialize, 0);
   rb_define_method(rb_cIR, "sendIr", meth_sendIr, 1);
+  rb_define_method(rb_cIR, "recvIr", meth_recvIr, 0);
 }
